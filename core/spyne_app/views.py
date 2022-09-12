5d94d1e63daa89a12d711b078aa1650cde23a454
shakhnoza-i <shaha0111@mail.ru>
2022-09-12 18:28:30 +0600

    explanation is added

--- core/spyne_app/views.py
+++ core/spyne_app/views.py
@@ -8,19 +8,25 @@ from spyne.service import ServiceBase
 
 
 class SoapService(ServiceBase):
+    # rpc is method decorator to tag a method as a remote procedure call 
+    # in a spyne.service.ServiceBase subclass.
+
     # There is one input parameter and parse it as a Unicode string, cannot be null; 
     # there output is a Unicode string, too.
     @rpc(Unicode(nillable=False), _returns=Unicode)
     def hello(ctx, name):
         return 'Hello, {}'.format(name)
 
-    # There are 2 input paramters and both of them are not null integers, 
+    # There are 2 input parameters and both of them are not null integers, 
     # the output is also an integer.
     @rpc(Integer(nillable=False), Integer(nillable=False), _returns=Integer)
     def sum(ctx, a, b):
         return int(a + b)
 
 
+# A SOAP application should be created to wrap SOAP services, the services should be 
+# listed as the first argument of spyne.application.Application constructor, base on 
+# our implementation the list is [SoapService].
 soap_app = Application(
     [SoapService],
     tns='django.soap.example',
