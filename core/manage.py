6fd4a37fe2f55948eb034e0c1e0e746a89859555
shaha <shaha0111@mail.ru>
2022-09-19 23:43:03 +0600

    const redefining

--- core/manage.py
+++ core/manage.py
@@ -2,7 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import spyne.const
 
+
+spyne.const.RESPONSE_SUFFIX = 'ww'
 
 def main():
     """Run administrative tasks."""
